934815a2619752f33df70da0b329243dffa42e4e
Rizkyramdani <rizkyramdani.rr05@gmail.com>
2022-01-06 06:11:41 +0700

    Menyelesaikan video view pertemuan4

--- Pertemuan4/app/controllers/About.php
+++ Pertemuan4/app/controllers/About.php
@@ -1,9 +1,9 @@
 <?php
 
 class About {
-    public function index($nama = 'Ramdani', $pekerjaan = 'Musisi') {
-
-        echo "Halo, nama saya $nama, saya adalah seorang $pekerjaan";
+    public function index($nama = 'Ramdani', $pekerjaan = 'Musisi', $umur = 34) 
+    {
+        echo "Halo, nama saya $nama, saya adalah seorang $pekerjaan, saya berumur $umur";
     }
     public function page() {
         
